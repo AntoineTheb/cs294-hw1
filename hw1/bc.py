05391a83e64ea6cdff590a25022cabd9e2eece9d
AntoineTheb <theberge.antoine.cem@gmail.com>
2018-09-29 20:10:28 -0400

    hw1 bc done

---
+++ hw1/bc.py
@@ -0,0 +1,82 @@
+from __future__ import absolute_import, division, print_function
+
+import os
+import pickle
+import tensorflow as tf
+from tensorflow import keras
+from keras import backend as K
+import numpy as np
+import tf_util
+import gym
+import load_policy
+import matplotlib.pyplot as plt
+from policy import *
+
+def get_experts_data(expert_file, envname, render, max_timesteps, num_rollouts):
+    print('loading and building expert policy')
+    policy_fn = load_policy.load_policy(expert_file)
+    print('loaded and built')
+    expert_data = {}
+    env = gym.make(envname)
+    max_steps = max_timesteps or env.spec.timestep_limit
+    returns = []
+    observations = []
+    actions = []
+
+    for i in range(num_rollouts):
+        # print('iter', i)
+        obs = env.reset()
+        totalr = 0
+        steps = 0
+        done = False
+        while not done:
+            action = policy_fn(obs[None,:])[0]
+            observations.append(obs)
+            actions.append(action)
+            obs, r, done, _ = env.step(action)
+            totalr += r
+            steps += 1
+
+        returns.append(totalr)
+
+    # print('returns', returns)
+    print('mean return', np.mean(returns))
+    print('std of return', np.std(returns))
+
+
+    expert_data['observations'] = np.array(observations)
+    expert_data['actions'] = np.array(actions)
+
+    #print('Training data', expert_data['observations'].shape)
+    #print('Format', expert_data['observations'][0])
+    #print('Training labels', expert_data['actions'].shape)
+    #print('Format', expert_data['actions'][0])
+    return expert_data
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('expert_policy_file', type=str)
+    parser.add_argument('envname', type=str)
+    parser.add_argument('--render', action='store_true')
+    parser.add_argument("--max_timesteps", type=int)
+    parser.add_argument('--num_rollouts', type=int, default=20,
+                        help='Number of expert roll outs')
+    args = parser.parse_args()
+    with tf.Session():
+        tf_util.initialize()
+
+        expert_data = get_experts_data(args.expert_policy_file,
+            args.envname,
+            args.render,
+            args.max_timesteps,
+            args.num_rollouts)
+
+        num_test = int(expert_data['observations'].shape[0] / 10)
+
+        model = train_model(expert_data, num_test)
+        plt.show()
+        play(model, args.envname, args.max_timesteps, args.num_rollouts)
+
+if __name__ == '__main__':
+    main()
---
+++ hw1/policy.py
@@ -0,0 +1,102 @@
+from __future__ import absolute_import, division, print_function
+
+import os
+import pickle
+import tensorflow as tf
+from tensorflow import keras
+from keras import backend as K
+import numpy as np
+import tf_util
+import gym
+import load_policy
+import matplotlib.pyplot as plt
+
+def plot_history(history):
+    plt.figure()
+    plt.xlabel('Epoch')
+    plt.ylabel('Mean Abs Error')
+    plt.plot(history.epoch, np.array(history.history['mean_absolute_error']),
+             label='Train Loss')
+    plt.plot(history.epoch, np.array(history.history['val_mean_absolute_error']),
+             label = 'Val loss')
+    plt.legend()
+    plt.ylim([0, 1])
+
+
+
+class PrintDot(keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs):
+        if epoch % 100 == 0:
+           print('')
+        print('.', end='')
+
+def build_model(train_data, train_label):
+    model = keras.Sequential([
+    keras.layers.Dense(64, activation=tf.nn.relu,
+    input_shape=(train_data.shape[1],)),
+    keras.layers.Dense(64, activation=tf.nn.relu),
+    keras.layers.Dense(train_label.shape[1])
+    ])
+    optimizer = tf.train.RMSPropOptimizer(0.001)
+    model.compile(loss='mse',
+    optimizer=optimizer,
+    metrics=['mae'])
+    return model
+
+
+def train_model(expert_data, test_data_size):
+    train_data = expert_data['observations'][test_data_size:]
+    test_data = expert_data['observations'][:test_data_size]
+    train_label = expert_data['actions'][test_data_size:]
+    test_label = expert_data['actions'][:test_data_size]
+
+    model = build_model(train_data, train_label)
+    # model.summary()
+
+    EPOCHS = 500
+
+    # Store training stats
+    history = model.fit(train_data, train_label, epochs=EPOCHS,
+            validation_split=0.2, verbose=0,
+            callbacks=[PrintDot()])
+
+    plot_history(history)
+
+    [loss, mae] = model.evaluate(test_data, test_label, verbose=0)
+    print("Testing set Mean Abs Error: ${:7.2f}".format(mae * 1000))
+
+    return model
+
+def play(policy, envname, max_timesteps, num_rollouts):
+    env = gym.make(envname)
+    max_steps = max_timesteps or env.spec.timestep_limit
+    returns = []
+    observations = []
+    actions = []
+
+    for i in range(num_rollouts):
+        # print('iter', i)
+        obs = env.reset()
+        totalr = 0
+        steps = 0
+        done = False
+        while True:
+            env.render()
+            action = policy.predict(obs[None,:])[0]
+            #obs, r, done, _ = env.step(env.action_space.sample())
+            obs, r, done, _ = env.step(action)
+            totalr += r
+            steps += 1
+            # if steps % 100 == 0: print("%i/%i"%(steps, max_steps))
+            if steps >= max_steps:
+                # print('Done after {} steps'.format(steps))
+                # print('Total reward', totalr)
+                break
+
+        returns.append(totalr)
+
+    # print('returns', returns)
+    print('mean return', np.mean(returns))
+    print('std of return', np.std(returns))
+
+
